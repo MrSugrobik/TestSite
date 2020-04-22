ee8ac683b837b39cc4a207ac763d73c4d635ca91
Andrey_K <mrsugrobik@gmail.com>
2020-04-22 22:26:06 +0300

    Переделаны меню из div в ul

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$(".header_burger").click(function(event) {
-		$(".header_burger, .element_container").toggleClass("active");
+		$(".header_burger, .header_elements").toggleClass("active");
 		$("body").toggleClass("lock");
 	});
 });
--- work/script/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	$(".header_burger").click(function(event) {
-		$(".header_burger, .header_elements").toggleClass("active");
-		$("body").toggleClass("lock");
-	});
-});
